1f2629f3cee905305a44aa09a91f0b9689262840
sr <srzzuares@gmail.com>
2023-07-03 21:54:05 -0600

    code and updated

--- src/Controllers/paypal.controller.js
+++ src/Controllers/paypal.controller.js
@@ -1,6 +1,37 @@
+import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config()
 const payCtll = {}
+const PALPAL_API= process.env.PAYPAL_API
 
-payCtll.orderCreate = (req,res) => {res.send('this in Order Created')}
+payCtll.orderCreate = (req,res) => {
+    const order = {
+        intent: "CAPTURE",
+        purchase_units:[
+            {
+                amount: {
+                    currecy_code:"USD",
+                    value: "100.00"
+                }
+            },
+            {
+                amount: {
+                    currecy_code:"EUR",
+                    value: "100.00"
+                }
+            }
+        ],
+        application_context:{
+            brand_name: "CrySuar",
+            landing_page: "NO_REFERENCE",
+            user_action: "PAY_NOW",
+            return_url: `http://localhost:3033/capture`,
+            cancel_url: `http://localhost:3033/canceled`
+        }
+    }
+    axios.post(`${PALPAL_API}/v2/checkout/orders`)
+
+}
 payCtll.orderCapture = (req,res) => {res.send('This in Capture Order')}
 payCtll.orderCanceled = (req,res) => {res.send('This in Order Canceled')}
 export default payCtll
--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import payRoute from './Routers/paypal.routes.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const PAYPAL_APY_KEY = process.env.PAYPAL_APY_KEY
+const PALPAL_CLIENTID = process.env.PAYPAL_CLIENTID
+const PALPAL_API= process.env.PAYPAL_API
 const RunApp = express()
 
 RunApp.set('PORT', process.env.TZ_PORT || 3000)
